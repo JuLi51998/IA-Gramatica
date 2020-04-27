6786a602d6c9dc1fb27db3fd38b9e3a002e65036
Julian Goemz <gomezjulian51998@gmail.com>
2020-04-27 17:48:16 -0500

    Cambios en la logica de analisis

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function comprobar() {
+/*function comprobar() {
     var numA;
     var numB;
     var numC;
@@ -6,7 +6,7 @@ function comprobar() {
 
     var palabra = document.getElementById('palabra').value;//"aabbccaccc"
 
-    var exrA = /a*/;
+    var exrA = /a;
     var exrB = /b+(?=c)/;
     var exrPalabra =/^aa*(?=b)b+(?=c)[a-cA-C]*c$/;
 
@@ -21,12 +21,12 @@ function comprobar() {
         res = ((2*numA)+ numB);
         if(numC == res) {
             console.log('Correcto!');
-        } else console.log('Error!')
+        } else console.log('Error!');
     } else {
         alert("Error!");
     }
     
-}
+}*/
 
 function generar() {
     var a,b,c,n,m;
@@ -46,14 +46,90 @@ function generar() {
     
     a = 'a'.repeat(n);
     b = 'b'.repeat(m);
-    c = 'c'.repeat(2*n +m);
-
-    if( c%2 == 0) {
-
-    }
-
+    c = 'c'.repeat(2*n + im);
 
     res.innerHTML = a+b+c;
+}
+
+function automataFinito() {
+    var palabra = document.getElementById('palabra').value;
+    var inicial = 0;
+    var final = 3;
+    var actual = inicial;
+    var contador = 0
+    var fin = false;
+    var as = 0,bs = 0,cs = 0, res;
+
+    palabra = palabra.split("");
+
+    while(fin == false) {
+        if(actual == 0) {
+            if(palabra[contador] == "a") {
+                actual = 1;
+                as++;
+            } if(palabra[contador] == "b") {
+                actual = 0;
+                bs++;
+            } if(palabra[contador] == "c") {
+                actual = 0;
+                cs++;
+            }
+            contador++;
+        }
+        if(actual == 1) {
+            if(palabra[contador] == "a") {
+                actual = 1;
+                as++;
+            } if(palabra[contador] == "b") {
+                actual = 2;
+                bs++;
+            } if(palabra[contador] == "c") {
+                actual = 0;
+                cs++;
+            }
+            contador++;
+        }
+        if(actual == 2) {
+            if(palabra[contador] == "a") {
+                actual = 0;
+                as++;
+            } if(palabra[contador] == "b") {
+                actual = 2;
+                bs++;
+            } if(palabra[contador] == "c") {
+                actual = 3;
+                cs++;
+            }
+            contador++;
+        }
+        if(actual == 3) {
+            if(palabra[contador] == "a") {
+                actual = 0;
+                as++;
+            } if(palabra[contador] == "b") {
+                actual = 0;
+                bs++;
+            } if(palabra[contador] == "c") {
+                actual = 3;
+                cs++;
+            }
+            contador++;
+        }
+        if (contador > palabra.length - 1) {
+            fin = true;
+        }
+    }
+
+    res = ((2*as)+ bs);
+
+    if(actual == final && cs == res) {
+        console.log("La cadena es correcta!");
+        console.log("las a: ", as , "las b: ", bs, "las c: ", cs);
+    } else {
+        console.log("La cadena no es valida!");
+        console.log("las a: ", as , "las b: ", bs, "las c: ", cs);
+    }
+
 }
 
 // (1) S-> WTU (2) W -> Wa (3) T -> bT (4) U -> cUc (5) W -> a (6) T -> b (7) U -> c (8) U -> cU
